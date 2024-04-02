2787c6ba318f6931041cf89d32cd349c3fee0747
solaleh81 <solalehalizadeh81.com>
2024-04-03 00:21:58 +0330

    add: DEFAULT_AUTHENTICATION_CLASSES & DEFAULT_PERMISSION_CLASSES to REST_FRAMEWORK.

--- ganarcrm_django/settings.py
+++ ganarcrm_django/settings.py
@@ -30,6 +30,15 @@ ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication'
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 # Application definition
 
 INSTALLED_APPS = [
