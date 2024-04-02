cec43a7c7eba2b5aae4e5e0edc627c8623d08861
solaleh81 <solalehalizadeh81.com>
2024-04-03 00:10:38 +0330

    add: LeadViewSet.

--- lead/views.py
+++ lead/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Lead
+from .serializers import LeadSerializer
 
-# Create your views here.
+class LeadViewSet(viewsets.ModelViewSet):
+    serializer_class = LeadSerializer
+    queryset = Lead.objects.all()
+
+    def get_queryset(self):
+        return self.queryset.filter(created_by=self.request.user)
