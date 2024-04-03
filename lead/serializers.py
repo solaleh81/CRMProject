ca56b908e56ce63dd2b500201f57972dcdb29f52
solaleh81 <solalehalizadeh81.com>
2024-04-03 13:17:26 +0330

    add: read_only_fields to LeadSerializer.

--- lead/serializers.py
+++ lead/serializers.py
@@ -5,9 +5,14 @@ from rest_framework import serializers
 class LeadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lead
+        read_only_fields = (
+            'created_by',
+            'created_at',
+            'modified_at',
+        )
         fields = (
             'company',
-            'contact_person'
+            'contact_person',
             'email',
             'phone',
             'website',
@@ -15,7 +20,5 @@ class LeadSerializer(serializers.ModelSerializer):
             'estimated_value',
             'status',
             'priority',
-            'created_by',
-            'created_at',
-            'modified_at',
+
         )
