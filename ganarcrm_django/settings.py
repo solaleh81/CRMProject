5d0cf55888f44f8b30db7eb1078efc9c89aa12a1
solaleh81 <solalehalizadeh81.com>
2024-04-03 13:16:13 +0330

    add: CORS_ALLOWED_ORIGINS.

--- ganarcrm_django/settings.py
+++ ganarcrm_django/settings.py
@@ -31,14 +31,14 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000'
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication'
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    )
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework.authentication.TokenAuthentication',
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAuthenticated',
+#     )
+# }
 # Application definition
 
 INSTALLED_APPS = [
